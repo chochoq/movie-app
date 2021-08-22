2e4fd117a6a1926913f1f0a34040d59064b51b8d
chochoq <aubepe@gmail.com>
2021-08-22 22:45:23 +0900

    (add) movies axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
   state = {
@@ -6,10 +7,12 @@ class App extends React.Component {
     movies: [],
   };
 
+  getMovies = async() => {
+    const movies = await axios.get('https://yts-proxy.now.sh//list_movies.json');
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ is_loading: false });
-    }, 6000);
+    this.getMovies();
   }
 
   render() {
