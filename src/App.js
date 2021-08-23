325e3bbf9e0de1a916b424788292989075876ff5
chochoq <aubepe@gmail.com>
2021-08-23 22:45:13 +0900

    (add) rendering Movie.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
 
 class App extends React.Component {
   state = {
@@ -7,22 +8,31 @@ class App extends React.Component {
     movies: [],
   };
 
-  getMovies = async() => {
-    const movies = await axios.get('https://yts-proxy.now.sh//list_movies.json');
-  }
+  getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies:movies, is_loading:false });
+  };
 
   componentDidMount() {
     this.getMovies();
-  }
+  };
 
   render() {
-    const { is_loading } = this.state;
+    const { is_loading, movies, } = this.state;
     return (
       <div>
-        {is_loading ? 'loading...': 'ready!'}
+        {is_loading ? 'loading...' : movies.map(movie => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            poster={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary} />)
+        )}
       </div>
     )
-  }
+  };
 }
 
 export default App;
