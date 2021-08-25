f82c2591563d9423ee1b54f879dc94418cdee7b7
chochoq <aubepe@gmail.com>
2021-08-25 15:10:43 +0900

    (move) 컴포넌트이동 밎 정리

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import Home from './routes/Home';
 
 
-class App extends React.Component {
-  state = {
-    is_loading: false,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {data:{data:{movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies:movies, is_loading:false });
-  };
-
-  componentDidMount() {
-    this.getMovies();
-  };
-
-  render() {
-    const { is_loading, movies, } = this.state;
-    return (
-      <section className="container">
-        {is_loading ?(
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-            <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                poster={movie.medium_cover_image}
-                title={movie.title}
-                genres={movie.genres}
-                summary={movie.summary}
-              />
-              ))
-            }
-          </div>
-        )}
-      </section>
-    )
-  };
+function App() {
+  return <div>
+    <Home/>
+  </div>
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
+
+
+class Home extends React.Component {
+    state = {
+    is_loading: false,
+    movies: [],
+    };
+
+    getMovies = async () => {
+    const {data:{data:{movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
+    this.setState({ movies:movies, is_loading:false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  };
+
+  render() {
+    const { is_loading, movies, } = this.state;
+    return (
+      <section className="container">
+        {is_loading ?(
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+        ) : (
+            <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                poster={movie.medium_cover_image}
+                title={movie.title}
+                genres={movie.genres}
+                summary={movie.summary}
+              />
+              ))
+            }
+          </div>
+        )}
+      </section>
+    )
+  };
+}
+
+export default Home;
