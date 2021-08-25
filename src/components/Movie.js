c39f3649463b3efc1327db3784278c168c30de21
chochoq <aubepe@gmail.com>
2021-08-25 19:12:11 +0900

    (add) movie.js -> props

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom';
 import './Movie.css';
 
-function Movie({id, title,year,summary,poster, genres}) {
+function Movie({ id, title, year, summary, poster, genres }) {
+    
     return (
         <div className="movie">
-            <img src={poster} alt={title} title={title} />
-            <div className="movie_data">
-                <h3 className="movie_title">{title}</h3>
-                <h5 className="movie_year">{year}</h5>
-                <p className="movie_genres">
-                    {genres.map((genre, idx) => (
-                        <li className="genres_genres" key={idx}>{genre}</li>
-                ))}</p>
-                <h5 className="movie_summary">{summary.slice(0, 140)}...</h5>
-            </div>
+            <Link
+                to={{
+                    pathname: `/detail/${id}`,
+                    state: {
+                        year: year,
+                        title: title,
+                        summary: summary,
+                        poster: poster,
+                        genres: genres
+                    }   
+                }}
+            >
+            
+                <img src={poster} alt={title} title={title} />
+                <div className="movie_data">
+                    <h3 className="movie_title">{title}</h3>
+                    <h5 className="movie_year">{year}</h5>
+                    <p className="movie_genres">
+                        {genres.map((genre, idx) => (
+                            <li className="genres_genres" key={idx}>{genre}</li>
+                    ))}</p>
+                    <h5 className="movie_summary">{summary.slice(0, 140)}...</h5>
+                </div>
+            </Link>
         </div>
     )
 }
