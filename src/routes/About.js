64d38a3510c1d6ed0889543776c44deaca5ea6a4
chochoq <aubepe@gmail.com>
2021-08-25 19:12:29 +0900

    (add) About

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './About.css';
 
-function About() {
+function About(props) {
+    console.log(props);
     return (
-        <h1>kind of details.. i think</h1>
+        <div className="movie_about">
+            <h1>영화리스트만들기 빠잉</h1>
+        </div>
     );
 }
 
